135c2ff439349706245483dc68f460ed9b81a544
Peter8234 <peter@webapp.io>
2021-09-08 00:48:44 -0400

    remove sudo

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 # exposes docker for local environment 
-
 # Create the directory to store the configuration file.
-sudo mkdir -p /etc/systemd/system/docker.service.d
+mkdir -p /etc/systemd/system/docker.service.d
 # Create a new file to store the daemon options.
-sudo echo '[Service]
+echo '[Service]
 ExecStart=
 ExecStart=/usr/bin/dockerd -H unix:// -H tcp://0.0.0.0:2375' > /etc/systemd/system/docker.service.d/options.conf
 # Reload the systemd daemon.
-sudo systemctl daemon-reload
+systemctl daemon-reload
 # Restart Docker.
-sudo systemctl restart docker
+systemctl restart docker
 
 echo "done docker setup"
